693c34c745a109a2efa5a041ef28bd43f0973662
ADEL\spp109 <martin.magnusson@adelfors.nu>
2020-09-04 09:52:35 +0200

    first init

---
+++ src/RPGClassSystem/Core/Stats/StatsSystem.cs
@@ -0,0 +1,18 @@
+﻿using RPGClassSystem.Core.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGClassSystem.Core.Stats
+{
+	public class StatsSystem
+	{
+		public Dictionary<UnitStats, int> Stats { get; } =
+			new Dictionary<UnitStats, int>()
+			{
+				{ UnitStats.Health, 0 }
+			};
+	}
+}
---
+++ src/RPGClassSystem/Core/Units/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGClassSystem.Core.Units
+{
+	class Unit
+	{
+	}
+
+	public enum UnitStats
+	{
+		Health,
+		Dexterity,
+		Agility,
+		Strength
+	}
+}
